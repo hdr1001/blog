5f4b3941ef522b8d769933a277a69621894cbf50
Hans de Rooij <HansDeRooij@Hotmail.com>
2020-08-26 17:39:12 +0200

    Introduced navigation object in JS code

--- js/hdrblogv2.js
+++ js/hdrblogv2.js
@@ -1,12 +1,60 @@
 'use strict'
 
-const blogProtocol = 'https://';
-const blogDomain = 'hdr1001.github.io/';
-const blogPath = 'blog/';
-const blogContentPath = 'src/';
-const blogContentQryParam = 'content';
-let blogContent = 'first_post.html'
+//Object containing & exposing blog parameters
+const blog = {
+   protocol: 'https://',
+   domain: 'hdr1001.github.io/',
+   path: 'blog/',
+   contentPath: 'src/',
+   javaScript: 'js/',
+   defaultArticle: 'l_halliday_blog.html',
+   contentQryParam: 'content',
+   articleList: 'blog_articles.json',
 
+   getBlogArticleUrl: function() {
+      return this.protocol + this.domain + this.path + this.contentPath;
+   },
+
+   getBlogArticleListUrl: function() {
+      return this.protocol + this.domain + this.path + this.javaScript + this.articleList;
+   }
+};
+
+//Constructor function for blog navigation object
+function BlogNav() {
+   this.blogCurrIdx = 0;
+   this.arrArticles = null;
+
+   //HTTP request to retrieve the article array
+   const xhr = new XMLHttpRequest();
+
+   //Format of the content retrieved is JSON
+   xhr.responseType = 'json';
+
+   xhr.open('GET', blog.getBlogArticleListUrl()); //Fetch the data
+
+   xhr.onload = () => {
+      if (xhr.status != 200) { //Record HTTP (exception) status
+         console.log(`Error rerieving article list: HTTP status ${xhr.status}, ${xhr.statusText}`);
+      }
+      else { //Success
+         console.log('Successfully loaded the requested article list');
+
+         //The article list can be accessed via oArticles
+         this.arrArticles = xhr.response.hdr_blog_idx;
+
+         this.arrArticles.forEach(article => {
+            console.log(article.title)
+         });
+      }
+   };
+
+   xhr.onerror = () => { //Technical network error
+      console.log('Technical network error occured while retrieving article list');
+   };
+ 
+   xhr.send();
+}
 //Get the value of a URL query string parameter
 function getUrlQryStrParamVal(qryStrParam) {
    qryStrParam = qryStrParam.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -29,21 +77,22 @@ function updateBlogPost() {
       return;
    }
 
-   const sURL = blogProtocol + blogDomain + blogPath + blogContentPath + blogContent;
-
+   //Old school HTTP request
    const xhr = new XMLHttpRequest();
 
+   //Format of the content is plain text
    xhr.responseType = 'text';
 
-   xhr.open('GET', sURL);
+   xhr.open('GET', blog.getBlogArticleUrl() + blog.defaultArticle);
 
    xhr.onload = () => {
       if (xhr.status != 200) { //Record HTTP (exception) status
-         article.innerHTML = `<p>Error ${xhr.status}: ${xhr.statusText}</p>`;
+         article.innerHTML = `<p>Error rerieving blog post: HTTP status ${xhr.status}, ${xhr.statusText}</p>`;
       }
       else { //Success
          console.log('Successfully retrieved the requested blog post');
 
+         //Replace the existing DOM content
          article.innerHTML = xhr.response;
       }
    };
@@ -58,5 +107,24 @@ function updateBlogPost() {
 document.addEventListener('DOMContentLoaded', event => {
    console.log('Hosted on URL ' + window.location.pathname);
 
+   let blogNav = new BlogNav;
+
    updateBlogPost();
+
+   document.querySelector('.blog_index').addEventListener('click', evnt => {
+      console.log(evnt.target.className);
+      evnt.preventDefault();
+   });
+   document.querySelector('.blog_prev').addEventListener('click', evnt => {
+      console.log(evnt.target.className);
+      evnt.preventDefault();
+   });
+   document.querySelector('.blog_next').addEventListener('click', evnt => {
+      console.log(evnt.target.className);
+      evnt.preventDefault();
+   });
+   document.querySelector('.blog_last').addEventListener('click', evnt => {
+      console.log(evnt.target.className);
+      evnt.preventDefault();
+   });
 });

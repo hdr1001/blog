5080fddb8e15f6a3d4574c5a72598f03c2f64457
Hans de Rooij <HansDeRooij@Hotmail.com>
2020-08-29 11:08:43 +0200

    Added basic navigation

--- js/hdrblogv2.js
+++ js/hdrblogv2.js
@@ -36,44 +36,7 @@ function BlogNav() {
    //Object instance properties
    this.blogCurrIdx = 0;
    this.arrArticles = null;
-
-   //Change the article displayed in DOM node blog_post
-   function switchBlogPost() {
-      //Get a reference to the article node
-      const elemArticle = document.getElementById('blog_post');
-
-      //Exit if the article node is not present
-      if(!elemArticle) {
-         console.log('getElementById on identifier \'blog_post\' failed');
-         return;
-      }
-
-      //Old school HTTP request
-      const xhr = new XMLHttpRequest();
-
-      //Format of the content is plain text
-      xhr.responseType = 'text';
-
-      xhr.open('GET', blog.getBlogArticleUrl() + blog.defaultArticle);
-
-      xhr.onload = () => {
-         if (xhr.status != 200) { //Record HTTP (exception) status
-            elemArticle.innerHTML = `<p>Error rerieving blog post: HTTP status ${xhr.status}, ${xhr.statusText}</p>`;
-         }
-         else { //Success
-            console.log('Successfully retrieved the requested blog post');
-
-            //Replace the existing DOM content
-            elemArticle.innerHTML = xhr.response;
-         }
-      };
-
-      xhr.onerror = () => { //Technical network error
-         elemArticle.innerHTML = '<p>Technical network error occurred</p>';
-      };
-   
-      xhr.send();
-   }
+   this.elemArticle = null;
 
    //HTTP request to retrieve the article array
    const xhr = new XMLHttpRequest();
@@ -93,7 +56,7 @@ function BlogNav() {
          //The article list can be accessed via oArticles
          this.arrArticles = xhr.response.hdr_blog_idx;
 
-         switchBlogPost();
+         this.switchBlogPost(0);
       }
    };
 
@@ -104,6 +67,88 @@ function BlogNav() {
    xhr.send();
 }
 
+//BlogNav member function to change the article displayed in DOM node blog_post
+BlogNav.prototype.switchBlogPost = function(newIdx) {
+   //Get a reference to the article node
+   if(!this.elemArticle) {
+      this.elemArticle = document.getElementById('blog_post');
+
+      //Exit if the article node is not present
+      if(!this.elemArticle) {
+         console.log('getElementById on identifier \'blog_post\' failed');
+         return;
+      }
+   }
+
+   //Get the file name of the blog post to retrieve
+   let sFileNamePost = this.arrArticles[newIdx] && this.arrArticles[newIdx].file;
+
+   if(!sFileNamePost) {
+      console.log('Unable to establish file name of post @ index ' + newIdx);
+      return;
+   }
+
+   //Old school HTTP request
+   const xhr = new XMLHttpRequest();
+
+   //Format of the content is plain text
+   xhr.responseType = 'text';
+
+   xhr.open('GET', blog.getBlogArticleUrl() + sFileNamePost);
+
+   xhr.onload = () => {
+      if (xhr.status != 200) { //Record HTTP (exception) status
+         this.elemArticle.innerHTML = `<p>Error rerieving blog post: HTTP status ${xhr.status}, ${xhr.statusText}</p>`;
+      }
+      else { //Success
+         console.log('Successfully retrieved the requested blog post');
+
+         //Replace the existing DOM content
+         this.elemArticle.innerHTML = xhr.response;
+
+         //Update the current index
+         this.blogCurrIdx = newIdx;
+      }
+   };
+
+   xhr.onerror = () => { //Technical network error
+      this.elemArticle.innerHTML = '<p>Technical network error occurred</p>';
+   };
+
+   xhr.send();
+}
+
+//Navigate to the next blog post
+BlogNav.prototype.next = function() {
+   if(this.arrArticles[this.blogCurrIdx + 1]) {
+      this.switchBlogPost(this.blogCurrIdx + 1)
+   }
+   else {
+      console.log('Can\'t navigate beyond the last blog post')
+   }
+}
+
+//Navigate to the previous blog post
+BlogNav.prototype.prev = function() {
+   if(this.blogCurrIdx - 1 >= 0) {
+      this.switchBlogPost(this.blogCurrIdx - 1)
+   }
+   else {
+      console.log('Can\'t navigate to a blog post before the 1st one')
+   }
+}
+
+//Navigate to the last blog post
+BlogNav.prototype.last = function() {
+   if(this.arrArticles.length) {
+      this.switchBlogPost(this.arrArticles.length - 1)
+   }
+   else {
+      console.log('🤔, can\'t establish the articles array length')
+   }
+}
+
+//Further initialize the HTML page when the DOM is loaded
 document.addEventListener('DOMContentLoaded', event => {
    console.log('Hosted on URL ' + window.location.pathname);
 
@@ -113,28 +158,34 @@ document.addEventListener('DOMContentLoaded', event => {
    //Add a couple of event handlers
    document.querySelectorAll('.blog_index').forEach(elem => {
       elem.addEventListener('click', evnt => {
-         console.log(evnt.target.className);
+         console.log(evnt.target.className) + ' clicked';
          evnt.preventDefault();
       });
    });
 
    document.querySelectorAll('.blog_prev').forEach(elem => {
       elem.addEventListener('click', evnt => {
-         console.log(evnt.target.className);
+         console.log(evnt.target.className + ' clicked');
+         blogNav.prev();
+
          evnt.preventDefault();
       });
    });
 
    document.querySelectorAll('.blog_next').forEach(elem => {
       elem.addEventListener('click', evnt => {
-         console.log(evnt.target.className);
+         console.log(evnt.target.className + ' clicked');
+         blogNav.next();
+
          evnt.preventDefault();
       });
    });
 
    document.querySelectorAll('.blog_last').forEach(elem => {
       elem.addEventListener('click', evnt => {
-         console.log(evnt.target.className);
+         console.log(evnt.target.className + ' clicked');
+         blogNav.last();
+
          evnt.preventDefault();
       });
    });
